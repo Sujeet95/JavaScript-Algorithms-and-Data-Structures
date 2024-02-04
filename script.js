a41c0a92fd65e4ddc7b9e84dce2adc4421b90a78
Sujeet2658 <sujeetkumar778205@gmail.com>
2024-02-04 20:55:41 +0530

    modified html file and adding javascript file

---
+++ script.js
@@ -0,0 +1,16 @@
+// Soluation for JavaScript Program in freeCodeCamp...........
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+//Step 11- Declare two more variables named monsterHealth and inventory, but do not initialize them.
+let monsterHealth;
+let inventory;
+/**
+ * Step 12
+The variables you have assigned have all had values that are numbers. JavaScript has multiple different data types. The next one you will use is the string. Strings are used to store things like words or text. Strings are surrounded with double quotes, single quotes, or backticks. Here is an example of declaring a variable with a string:
+
+let developer = "Naomi";
+Assign the inventory variable to have the value of stick.
+ */
